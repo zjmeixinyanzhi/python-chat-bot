434f02ff048f782e4f862c06cada7bf3eb3a984d
Zhang Jie <zhangjiewanwansui@gmail.com>
2017-10-17 22:30:23 +0800

    fix play sound not finished bug

--- bot_voice_chat_bot.py
+++ bot_voice_chat_bot.py
@@ -16,20 +16,24 @@ if __name__ == '__main__':
         now = (time.time())
         print '机器人A:',ss1
         sound1 = 'D:/'+ str(now) +'A.mp3'
-        baidu_asr.to_sound(ss1,sound1,3)
-        time.sleep(1)
+        baidu_asr.to_sound(ss1,sound1,1)
+        
         track1 = pygame.mixer.music.load(sound1)
         pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == 1:
+            time.sleep(0.5)
+        
 
         ss = bot_2.do_chat(ss1)
         now = (time.time())
         print '机器人B:', ss
 
         sound2 = 'D:/' + str(now) + 'B.mp3'
-        baidu_asr.to_sound(ss,sound2,4)
-        time.sleep(1)
+        baidu_asr.to_sound(ss,sound2,0)
         track2 = pygame.mixer.music.load(sound2)
         pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == 1:
+            time.sleep(0.5)
 
         if ss1 == ss:
             count = count + 1
