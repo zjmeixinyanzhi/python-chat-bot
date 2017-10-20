237c11aa6caa7b5ec5b506a2c6052989da29e0ba
zhangjie <zhangjie@iscas.ac.cn>
2017-10-20 08:52:34 +0800

    finish sound composition

--- sound_composition.py
+++ sound_composition.py
@@ -4,30 +4,28 @@
 import os
 from pydub import AudioSegment
 
+AudioSegment.converter = "D:/ffmpeg.exe"
 def get_files(rootDir):
     files = []
     for lists in os.listdir(rootDir): 
         path = os.path.join(rootDir, lists) 
         files.append(path)
         if os.path.isdir(path): 
-            Test2(path)
+            (path)
     return files
 
 def do_composite(files):
     temp = files[0]
     print temp
-    enPath = "%s%s/%s"%(enDir,file,enfile) 
-    result =  AudioSegment.from_mp3(temp)
+    result = AudioSegment.from_mp3(temp)
     result.export('D:/all.mp3', format="mp3")
     del files[0]
     for s in files:
         curr = AudioSegment.from_mp3(s)
         result = result + curr  
-    result.export('D:/all.mp3', format="mp3")    
-
-
+        result.export('D:/all.mp3', format="mp3")
+        print s
 
 if __name__ == '__main__':
-
     sounds = get_files('D:/sound/')
     do_composite(sounds)
